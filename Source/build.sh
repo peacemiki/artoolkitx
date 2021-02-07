418a778b6a840292459577212446431cddb10b2b
SungshinPark <sinschool@gmail.com>
2021-02-07 14:05:55 +0900

    최신 소스로 업데이트  (이슈가 있어서 업데이트 함)
    최신 환경에서 빌드 결과로 나오는 .so 파일 크기가 너무 작아지고, 동작이 안됨.
    최신 소스로 업데이트하고 local.properties 에서 ndk 버전 지정하고,
    cmake 는 3.10.2.4988404 를 사용했음.

--- Source/build.sh
+++ Source/build.sh
@@ -26,7 +26,7 @@ fi
 set -e
 
 # -x = debug
-#set -x
+set -x
 
 # Parse parameters
 while test $# -gt 0
@@ -80,19 +80,14 @@ then
 elif [ "$OS" = "Darwin" ]
 then
     CPUS=`/usr/sbin/sysctl -n hw.ncpu`
-elif [ "$OS" = "CYGWIN_NT-6.1" ]
+elif [[ "$OS" == "CYGWIN_NT-"* ]]
 then
     # bash on Cygwin.
     CPUS=`/usr/bin/nproc`
     OS='Windows'
-elif [ "$OS" = "MINGW64_NT-6.1" ]
+elif [[ "$OS" == "MINGW64_NT-"* ]]
 then
-    # git-bash on Windows 7
-    CPUS=`/usr/bin/nproc`
-    OS='Windows'
-elif [ "$OS" = "MINGW64_NT-10.0" ]
-then
-    # git-bash on Windows.
+    # git-bash on Windows
     CPUS=`/usr/bin/nproc`
     OS='Windows'
 else
@@ -226,7 +221,7 @@ if [ -z "$ANDROID_HOME" ] ; then
 else
     echo "Building ARXJ library as AAR"
     cd $OURDIR
-    cd ARXJ/ARXJProj; ./gradlew -q assembleRelease;
+    cd ARXJ/ARXJProj; ./gradlew assembleRelease;
     cd $OURDIR
     mkdir -p ../SDK/lib/ARXJ/
     cp ARXJ/ARXJProj/arxj/build/outputs/aar/arxj-release.aar ../SDK/lib/ARXJ/
@@ -234,16 +229,16 @@ else
     if [ $BUILD_EXAMPLES ] ; then
         echo "Building example ARSquareTracking as APK"
         cd $OURDIR
-        cd "../Examples/Square tracking example/Android/ARSquareTracking"; ./gradlew -q assembleRelease;
+        cd "../Examples/Square tracking example/Android/ARSquareTracking"; ./gradlew assembleRelease;
         cd $OURDIR
-        cd "../Examples/Square tracking example with OSG/Android/ARSquareTracking"; ./gradlew -q assembleRelease;
+        cd "../Examples/Square tracking example with OSG/Android/ARSquareTracking"; ./gradlew assembleRelease;
         cd $OURDIR
         cp -v "../Examples/Square tracking example/Android/ARSquareTracking/ARSquareTrackingExample/build/outputs/apk/release/"ARSquareTrackingExample-release-unsigned.apk ../Examples/
         cp -v "../Examples/Square tracking example with OSG/Android/ARSquareTracking/ARSquareTrackingExample/build/outputs/apk/release/"ARSquareTrackingExample-release-unsigned.apk ../Examples/ARSquareTrackingExampleOSG-release-unsigned.apk
         
         echo "Building example AR2dTracking as APK"
         cd $OURDIR
-        cd "../Examples/2d tracking example/Android/AR2DTracking_Proj"; ./gradlew -q assembleRelease;
+        cd "../Examples/2d tracking example/Android/AR2DTracking_Proj"; ./gradlew assembleRelease;
         cd $OURDIR
         cp -v "../Examples/2d tracking example/Android/AR2DTracking_Proj/AR2DTrackingExample/build/outputs/apk/release/"AR2DTrackingExample-release-unsigned.apk ../Examples/
 
